c29be63ab668a2f91ebeb353e35049658357495e
techdevwizard <techdevwizard@gmail.com>
2025-06-14 17:17:03 +0530

    initial-commit

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -3,16 +3,9 @@ const router = express.Router();
 const authController = require('../controllers/AuthController');
 const authenticateToken = require('../middleware/AuthMiddleware');
 
-// Public
-router.post('/login', authController.loginUser);
 
-// Secured
+router.post('/login', authController.loginUser);
 router.get('/profile', authenticateToken, authController.getProfile);
 
 module.exports = router;
 
-// S- single responsibilty principle (SIP)
-// o-open closed principle(OCP)
-// l-liskov substitution principle(LSP)
-// i-interface segregation principle(ISP)
-// d-dependency inversion principle(DIP)
